0463f345acf7733960f53e477923c5a4ef18b8e1
geralldtit <geralldtit@gmail.com>
2020-03-18 01:03:56 +0300

    feat: implement all required functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {    
+  return (Array.isArray(array) && array.length ) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (Array.isArray(array) && array.length ) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (Array.isArray(array) && array.length ) ? calcAvg(array) : 0;  
+}
+
+function calcAvg(array){
+    const sum = array.reduce((a, b) => a + b, 0);
+    return (sum / array.length) || 0;
 }
